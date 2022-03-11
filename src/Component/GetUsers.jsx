ca3b1bce1bc8cf60b6a574202dd974dc5243313e
theerudito <erudito.tv@gmail.com>
2022-03-11 15:16:47 -0500

    v1

--- src/Component/GetUsers.jsx
+++ src/Component/GetUsers.jsx
@@ -1,9 +1,11 @@
 import React, { Fragment } from "react";
 import { useEffect } from "react";
-import { useState, } from "react";
+import { useState } from "react";
+
 import { GetApi } from "../Helpers/GetApiAxios";
-import { IconShowDown } from "../Icons/Icons";
+
 import "../Styles/card.css";
+import { CardApp } from "./Card";
 
 const inicialState = [
   {
@@ -21,7 +23,7 @@ const inicialState = [
 
 export const GetUsers = () => {
   const [users, setUsers] = useState(inicialState);
-  const [mostrarComponente, setMostrarComponente] = useState(false);
+
   //console.log(users[0]);
 
   useEffect(() => {
@@ -35,82 +37,10 @@ export const GetUsers = () => {
   };
 
   return (
-    <Fragment>
+    <>
       {users.map((item) => (
-        <div className="container">
-          <IconShowDown />
-
-
-          {/*Con un estado adicional le dicen cuando mostrarse o no*/}
-          <button onClick={() => setMostrarComponente(!mostrarComponente)}>
-            {/*Aqui solo cambio el texto de mi boton, para el ejemplo */}
-            {mostrarComponente ? `Ocultar` : `Mostrar`}
-          </button>
-
-
-          <div className="" id="card" key={item.id}>
-            {/* <h4>{item.id}</h4> */}
-            <img
-              src={item.pic}
-              alt="pic"
-              width="100px"
-              id="image"
-              className=""
-            />
-
-            <h2 className="" id="names">
-              {item.firstName.toUpperCase()} {item.lastName.toUpperCase()}
-            </h2>
-
-            <h4 className="" id="email">
-              Email: {item.email}
-            </h4>
-
-            <h4 className="" id="city">
-              City: {item.city}
-            </h4>
-
-            <h4 className="" id="company">
-              Company: {item.company}
-            </h4>
-
-            <h4 className="" id="skill">
-              Skill: {item.skill}
-            </h4>
-
-            <h4 className="" id="averages">
-              Averages: {item.grades.reduce((a, b) => a % b)}%
-            </h4>
-
-            <input
-              className="form-control"
-              type="text"
-              placeholder="Add Tag"
-              id="input"
-              onChange=""
-            ></input>
-
-            <button className="btn btn-primary" type="submit" id="button">
-              +
-            </button>
-          </div>
-
-
-          <div className={mostrarComponente ? "show-element" : null}>
-            {mostrarComponente && (
-              <div className="" id="test">
-                <h6>Test 1: {item.grades[0]}</h6>
-                <h6>Test 2: {item.grades[1]}</h6>
-                <h6>Test 3: {item.grades[2]}</h6>
-                <h6>Test 5: {item.grades[4]}</h6>
-                <h6>Test 6: {item.grades[5]}</h6>
-                <h6>Test 7: {item.grades[6]}</h6>
-              </div>
-            )}
-          </div>
-        </div>
+        <CardApp item={item} />
       ))}
-    </Fragment>
+    </>
   );
 };
-
