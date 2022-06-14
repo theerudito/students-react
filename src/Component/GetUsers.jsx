4a5a572666e8ee5be542297320ffbf8f46368aea
theerudito <erudito.tv@gmail.com>
2022-06-14 16:10:13 -0500

    v1

--- src/Component/GetUsers.jsx
+++ src/Component/GetUsers.jsx
@@ -1,24 +1,9 @@
-import { useContext, useEffect, useState } from "react";
+import { useContext } from "react";
 import UserContext from "../UserProvider/UserProvider";
 import { CardApp } from "./Card";
 
 export const GetUsers = () => {
-  const { serch, searchUser, results, tag } = useContext(UserContext);
-  const [searchTag, setSearchTag] = useState("");
-
-  const searchTagData = (e) => {
-    const results = !searchTag
-      ? tag
-      : tag.filter((dato) =>
-          dato.tag1.toLowerCase().includes(searchTag.toLocaleLowerCase())
-        );
-        console.log(results);
-      return results;
-  };
-
-  useEffect(() => {
-    searchTagData();
-  }, []);
+  const { serch, searchUser, results } = useContext(UserContext);
 
   return (
     <>
@@ -32,19 +17,9 @@ export const GetUsers = () => {
         />
       </div>
 
-      <div className="containerSeach">
-        <input
-          value={searchTag}
-          onChange={(e) => setSearchTag(e.target.value)}
-          type="text"
-          placeholder="Seach by Tag"
-          className="search"
-        />
-      </div>
-
       <div className="body">
         {results.map((item) => (
-          <CardApp item={item} key={item.id} dato={searchTagData} />
+          <CardApp item={item} key={item.id} />
         ))}
       </div>
     </>
